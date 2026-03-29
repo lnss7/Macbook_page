1e8a5ff1837a89f9f218a55b76ac9a6770d0835c
lnss7 <novellysimaolucas@gmail.com>
2026-03-29 20:45:07 -0300

    feat: initializing the highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,20 @@
 const Highlights = () => {
   return (
-    <div>Highlights</div>
+    <section id="highlights">
+        <h2>There's never been a better time to upgrade.</h2>
+        <h3>Here's what you get with the new MacBook Pro.</h3>
+
+        <div className="masonry">
+          <div className="left-column">
+            <div>
+              <img src="/laptop.png" alt="Laptop" />
+              <p>
+                Fly through demanding tasks up to 9.8x faster.
+              </p>
+            </div>
+          </div>
+        </div>
+    </section>
   )
 }
 
